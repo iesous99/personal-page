b8331379911ff4af64a5c42b389807ed33f13c7b
iesous blanco <iesous@iesouss-MacBook-Pro.local>
2025-06-13 16:02:39 -0400

    Updates

--- src/components/sections/footer/footer.tsx
+++
@@ -1,9 +0,0 @@
-import "./footer.css";
-
-export const Footer = () => {
-  return (
-    <footer className="footer">
-      <p className="footer__paragraph">Trabajemos juntos</p>
-    </footer>
-  );
-};
